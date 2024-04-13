5a0e8d25f67b7b998ded385400f7486685f3ae5f
Jefrsama <markups@trustme.kz>
2024-04-13 18:31:20 +0600

    updated router and deleted path for build

--- src/router.js
+++ src/router.js
@@ -1,24 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import About from '@/pages/aboutPage/about.vue';
 import Home from '@/pages/landing/landing.vue';
 
 const routes = [
     {
-        path: '/theme/space/dist/',
+        path: '/',
         name: 'Home',
         component: Home
     },
     {
-        path: '/theme/space/dist/about',
+        path: '/about',
         name: 'About',
         component: About
     }
 ];
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-
+export const router = createRouter({
+    history: createWebHashHistory(),
+    routes
 });
-
-export default router;
